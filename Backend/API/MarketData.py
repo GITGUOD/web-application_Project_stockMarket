6261ac2171f8c1a1f01b73b692e4d45782ecfec4
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-05-07 16:26:05 +0200

    Added a picture on our UML diagram

--- Backend/API/MarketData.py
+++ Backend/API/MarketData.py
@@ -1,9 +1,32 @@
-import yfinance as yf # Quick start
+import yfinance as yf # YFinance API, importing data
 import Database
+#API Reference: https://ranaroussi.github.io/yfinance/reference/index.html
+
 
 class MarketData:
+    #Utilizing our initial database
     def __init__(self, db: Database):
         self.db = db
+
+    def load_sample_tickers(self):
+        # Adding tickers and fetching company data using yfinance API
+        tickers = [("MSFT", "Microsoft Corporation"),
+           ("AAPL", "Apple Inc."),
+           ("GOOGL", "Alphabet Inc."),
+           ("TSLA", "Tesla Inc."),
+           ("NVDA", "Nvidia Corporation."),
+           ("AMZN", "Amazon.com Inc.")
+           ]
+        
+        for symbol in tickers:
+            try:
+                #Using all ticker objects in our vector and adding them to the database by fetching the method from our db class
+                ticker = yf.Ticker(symbol)
+                name = ticker.info.get("longName") or ticker.info.get("shortName") or "Unknown Company"
+                self.db.insert_ticker(symbol, name)
+            except Exception as e:
+                print(f"Failed to insert {symbol}: {e}")
+
 
 
 
@@ -23,12 +46,14 @@ print(data.head())
 
 print(data.columns)
 
-#We need a list of stocks:
+#A list of stocks:
 
 tickers = [("MSFT", "Microsoft Corporation"),
            ("AAPL", "Apple Inc."),
            ("GOOGL", "Alphabet Inc."),
-           ("TSLA", "Tesla Inc.")
+           ("TSLA", "Tesla Inc."),
+           ("NVDA", "Nvidia Corporation."),
+           ("AMZN", "Amazon.com Inc.")
            ]
 
 for symbol, name in tickers:
